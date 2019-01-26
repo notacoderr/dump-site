df53ff0b37b9c60a5f4b3b83d945cc1d0f696677
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-01-26 17:42:04 +0800

    Create phpDump.php

---
+++ phpDump.php
@@ -0,0 +1,34 @@
+<?php
+
+public function setText(string $name) {
+  foreach ($this->plugin->texts->getAll() as $level => $array) {
+   foreach ($array as $loc => $type) {
+    $pos = explode("_", $loc);
+      if(isset($pos[1])) {
+        $v3 = new Vector3((float) $pos[0],(float) $pos[1],(float) $pos[2]);
+          $this->plugin->addText($v3, $type, [$this->plugin->getServer()->getPlayerExact($name)]);
+      }
+    }
+  }
+}
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+public function addText(Vector3 $location, string $type = "title", $player = null) {
+		switch ($this->getServer()->getName()) {
+			case 'PocketMine-MP':
+				$typetitle = $this->config->get("texts-title")[$type];
+				$id = implode("_", [$location->getX(), $location->getY(), $location->getZ()]);
+				$particle = new FloatingTextParticle($location, color::GOLD . "<<<<<>>>>>", $this->colorize($typetitle) . "\n" . $this->getData($type));
+				$this->getServer()->getLevelByName($this->config->get("texts-world"))->addParticle($particle, $player);
+				$this->particles[$id] = $particle;
+				break;
+			case 'Altay':
+				$typetitle = $this->config->get("texts-title")[$type];
+				$id = implode("_", [$location->getX(), $location->getY(), $location->getZ()]);
+				$particle = new FloatingTextParticle(color::GOLD . "<<<<<>>>>>", $this->colorize($typetitle) . "\n" . $this->getData($type), $location);
+				$this->getServer()->getLevelByName($this->config->get("texts-world"))->addParticle($location, $particle);
+				$this->particles[$id] = $particle;
+				break;
+		}
+}
